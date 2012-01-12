7a1ccfc403d41530f83f053df301432f5b48de54
Mathieu Bolard <mattlawer08@gmail.com>
2012-01-12 22:07:52 +0100

    first commit

--- lib/siriproxy-frenchtest.rb
+++ lib/siriproxy-frenchtest.rb
@@ -1,5 +1,6 @@
 require 'cora'
 require 'siri_objects'
+require 'pp'
 
 #######
 # This is a "hello world" style plugin. It simply intercepts the phrase "test siri proxy" and responds
@@ -14,8 +15,20 @@ class SiriProxy::Plugin::Frenchtest < SiriProxy::Plugin
     #if you have custom configuration options, process them here!
   end
   
+  #get the user's location and display it in the logs
+  #filters are still in their early stages. Their interface may be modified
+  filter "SetRequestOrigin", direction: :from_iphone do |object|
+    puts "[Info - User Location] lat: #{object["properties"]["latitude"]}, long: #{object["properties"]["longitude"]}"
+    
+    #Note about returns from filters:
+    # - Return false to stop the object from being forwarded
+    # - Return a Hash to substitute or update the object
+    # - Return nil (or anything not a Hash or false) to have the object forwarded (along with any 
+    #    modifications made to it)
+  end 
+
   #demonstrate capturing data from the user (e.x. "Siri proxy number 15")
-  listen_for /calcul plus ([0-9,]+)/i do |number1|
+  listen_for /calcul plus ([0-9,]*[0-9])/i do |number1|
     say "Ca fait : #{number1}"
     
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
