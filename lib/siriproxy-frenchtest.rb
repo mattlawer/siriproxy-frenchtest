691261b287d8c04a95abce676e8b2eea9e434a96
Mathieu Bolard <mattlawer08@gmail.com>
2012-01-12 23:09:08 +0100

    first commit

--- lib/siriproxy-frenchtest.rb
+++ lib/siriproxy-frenchtest.rb
@@ -42,7 +42,7 @@ class SiriProxy::Plugin::FrenchTest < SiriProxy::Plugin
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
 
-  listen_for /([0-9,]*[0-9])/([0-9,]*[0-9])/i do |number1,number2|
+  listen_for /([0-9,]*[0-9])\/([0-9,]*[0-9])/i do |number1,number2|
     res = number1.to_i*number2.to_i
     say "Ca fait #{res}"
     
